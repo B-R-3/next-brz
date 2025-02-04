c64e67b948d228e24865f39bd643b9136961973d
koala819 <x.genolhac@gmail.com>
2025-02-04 15:18:01 +0100

    fix: display on HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 export default function Page()  {
   return (
-    <div className="text-center">
+    <header className="flex justify-center items-center mt-4">
+    <div className="text-center  bg-gray-200/90 p-8 shadow-xl rounded-lg w-1/2">
       <h1 className="text-3xl font-bold text-gray-900">Bienvenue chez Cazafamilia !</h1>
       <p className="text-gray-700 mt-4">
         Découvrez nos délicieux plats italiens faits maison 🍕🍝.
       </p>
     </div>
+    </header>
   );
 }
